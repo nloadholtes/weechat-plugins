470610227371722924cfee145733396803d20cc8
Nick Loadholtes <nick@ironboundsoftware.com>
2019-02-18 09:20:08 -0500

    Wrong function name

--- python/collapse.py
+++ python/collapse.py
@@ -23,5 +23,5 @@ def collapse_cb(data, modifier, modifier_data, string):
 if __name__ == "__main__":
     weechat.register("collapse_python", "Collapse", "1.0", "MIT",
                      "Collapse long messages (think github notifications) into more readable messages.", "collapse_end", "")
-    weechat.hook_modifier("irc_in_privmsg", "modifier_cb", "")
+    weechat.hook_modifier("irc_in_privmsg", "collapse_cb", "")
     weechat.prnt("", "Collapse (python) started")
