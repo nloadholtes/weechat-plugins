e806da155f422f69780429cb9966b4e4b097f682
Nick Loadholtes <nick@ironboundsoftware.com>
2019-02-26 18:17:12 -0500

    Getting closer

--- python/github.py
+++ python/github.py
@@ -18,14 +18,20 @@ def buffer_close_cb(data, buffer):
     return weechat.WEECHAT_RC_OK
 
 
-def gh_watch_cb(data, signal, signal_data):
-    server = signal.split(",")[0]
-    msg = weechat.info_get_hashtable("irc_message_parse", {"message": signal_data})
+def gh_watch_cb(data, bufferp, date, tags, disp, high, prefix, msg):
+    if prefix != "]":
+        return weechat.WEECHAT_RC_OK
+    
     buffer = weechat.buffer_search("python", "GithubWatcher")
     # print("In join_cb: %s " % msg)
     if buffer:
-        output = "%s (%s) has joined this channel?" % (msg["nick"], msg["channel"])
-        weechat.prnt(buffer, output)
+        # output = "%s (%s) has joined this channel?" % (msg["nick"], msg["channel"])
+        # prefix:  'github :]'
+        print("tags: %s " % tags)
+        print("disp: %s " % disp)
+        print("high: %s " % high)
+        print("prefix: %s" % prefix)
+        weechat.prnt(buffer, msg)
         # weechat.prnt_date_tags(buffer, 0, "", output)  # Throws down a datestamp
         weechat.buffer_set(buffer, "hotlist", "3")  # Sets the color of the bufferlist channel to magenta
     return weechat.WEECHAT_RC_OK
@@ -33,10 +39,10 @@ def gh_watch_cb(data, signal, signal_data):
 
 if __name__ == "__main__":
     weechat.register("github_watcher", "GithubWatcher", "1.0", "MIT",
-                     "Keeping an eye on github messages", "gw_watch_end", "")
+                     "Keeping an eye on github messages", "gh_watch_end", "")
     weechat.prnt("", "Github watcher (python) started")
     buffer = weechat.buffer_new("GithubWatcher", "buffer_input_cb", "", "buffer_close_cb", "")
     weechat.buffer_set(buffer, "title", "Watching Github messages")
     weechat.buffer_set(buffer, "localvar_set_no_log", "0")
 
-    weechat.hook_signal("*,irc_in2_join", "join_cb", "")
+    weechat.hook_print("", "", "", 0, "gh_watch_cb", "")
